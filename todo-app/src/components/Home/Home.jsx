6489098296c2afe3586c7c29a9112488c46c2018
Tomer <tomermi123@gmail.com>
2024-04-13 23:39:08 +0300

    Refactor Home component and Todo component

--- todo-app/src/components/Home/Home.jsx
+++ todo-app/src/components/Home/Home.jsx
@@ -5,7 +5,10 @@ export const Home = () => {
     return (
         <div className="home-wrap">
             <h1>T O D O </h1>
-            <TodoList />
+            <div className='toDoListAndCreate'>
+                <TodoList />
+            </div>
+            <span>Drag and drop to reorder list</span>
         </div>
     );
 };
--- todo-app/src/components/Todo/Todo.jsx
+++ todo-app/src/components/Todo/Todo.jsx
@@ -3,6 +3,10 @@ import { v4 as uuidv4 } from 'uuid';
 import { useDrag, useDrop } from "react-dnd";
 
 
-export const Todo = () => {
+export const Todo = ({todo, checkToggle, onDelete, index, moveTodo}) => {
 
+    const red = useRef(null);
+
+
+    
 };
