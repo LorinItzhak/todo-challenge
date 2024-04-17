ab1044045d29dae959c586822aa73c844b3e977e
LorinItzhak <lorinitzhak284@gmail.com>
2024-04-17 21:19:02 +0300

    active,completed,all buttons

--- TODO/src/App.jsx
+++ TODO/src/App.jsx
@@ -1,58 +1,107 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { List } from './components/List'
-import { Items } from './components/Items'
-import { FilterButtons } from './components/FilterButtons'
-import { TodoFrom } from './components/TodoForm'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import { List } from "./components/List";
+import { Items } from "./components/Items";
+import { FilterButtons } from "./components/FilterButtons";
+import { TodoFrom } from "./components/TodoForm";
+import "./App.css";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [showOnlyCompleted, setShowOnlyCompleted] = useState(false);
+  // const [showAllTodos, setShowAllTodos] = useState(true); 
+  const [showOnlyActive, setShowOnlyActive]=useState(false);
+
+  const todoToShow =
+    showOnlyCompleted === true
+      ? todos.filter((todo) => todo.completed === true)
+      : showOnlyActive === true 
+      ? todos.filter((todo) => todo.completed === false)
+      : todos;
+     
+
+  const onShowOnlyCompletedTodos = () => {
+    setShowOnlyCompleted(true);
+    setShowOnlyActive(false);
+  };
 
 
-function App () {
-   
-   const [todos, setTodos] = useState([]);
 
-   const addTodo= (text)=> {
+  const onShowActivetodos =()=>{
+    setShowOnlyCompleted(false);
+    setShowOnlyActive(true);
+  }
+
+  const onShowAll =()=>{
+    setShowOnlyCompleted(false);
+    setShowOnlyActive(false);
+
+  }
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      const res = await fetch(
+        "https://danielmargolin.github.io/TodoJSON/todosLorin.json"
+      ); // 5 sec
+      const todosFromServer = await res.json(); // 1 mil
+      setTodos(todosFromServer);
+    };
+    fetchTodos(); // fetch data from server
+  }, []);
+
+  const addTodo = (text) => {
     const newTodos = [...todos, { id: Date.now(), text, completed: false }];
 
-  
     setTodos(newTodos);
+  };
 
-   };
 
-   const toggleComplete=(id)=>{
-    const updateTodos = todos.map((todo)=>
-    todo.id === id ? {...todo,completed : !todo.completed}: todo
+  const toggleComplete = (id) => {
+    const updateTodos = todos.map((todo) =>
+      todo.id === id ? { ...todo, completed: !todo.completed } : todo
     );
 
     setTodos(updateTodos);
-   };
+  };
 
+ const deleteTodo = (id) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  };
 
-    const deleteTodo= (id)=> {
-      const updatedTodos = todos.filter((todo) => todo.id !== id);
-      setTodos(updatedTodos)
+  // const onShowOnlyCompletedTodos = () => {
+  //   setShowOnlyCompleted(true);
+  //   setShowAllTodos(false); // כאשר נלחץ כפתור הושלמו, נציב את המשתנה הזה ל־false
+  // };
 
-    };
+ 
+  return (
+    <>
+      <div className="App">
+        <h1>TODO</h1>
+        <TodoFrom className="todo-form" addTodo={addTodo} />
+        <List
+          todos={todoToShow}
+          toggleComplete={toggleComplete}
+          deleteTodo={deleteTodo}
+          onShowComplited={onShowOnlyCompletedTodos}
+          onShowAll={onShowAll}
+          onShowActive={onShowActivetodos}
+         
+        />
+        
+        
+      </div>
 
-   return(
-    <div className="App">
-      <h1>TODO</h1>
-      <TodoFrom className="todo-form" addTodo={addTodo}/>
-      <List todos={todos}
-      toggleComplete={toggleComplete}
-      deleteTodo={deleteTodo}
-      />
-    </div>
-
-   );
-
+      
+    </>
+  );
 }
 
-export default App
+export default App;
 
 
+  
 
-
-
-
+ 
--- TODO/src/components/List.jsx
+++ TODO/src/components/List.jsx
@@ -2,24 +2,35 @@ import React from "react";
 import { useState } from "react";
 import { Items } from "./Items";
 
-export function List ({todos, toggleComplete, deleteTodo}){
-
-  return(
+export function List({ todos, toggleComplete, deleteTodo ,onShowComplited,onShowAll,onShowActive}) {
+  return (
     <div className="todo-list">
-      {
-        todos.map((todo)=>(
-        <Items 
-        key={todo.id}
-        todo={todo}
-        toggleComplete={toggleComplete}
-        deleteTodo={deleteTodo}
-         />
-         ))}
+      <div className="list-container">
+      {todos.map((todo) => (
+        <Items
+          key={todo.id}
+          todo={todo}
+          toggleComplete={toggleComplete}
+          deleteTodo={deleteTodo}
+      
+        />
+      ))}
+      </div>
+      <button className="button-completed" onClick={onShowComplited}>
+        complited
+      </button>
+      <button className="button-All" onClick={onShowAll}>
+         All
+         </button>
+         <button className="button-Active" onClick={onShowActive}>
+          Active
+         </button>
+         <button className="button-Active" onClick={deleteTodo}>
+          ClearComplete
+         </button>
 
     </div>
   );
-
 }
 
 export default List;
-
